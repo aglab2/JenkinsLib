814142795d2f7d3b8b36179b1da32b49b721c80b
aglab2 <denis.kopyrin@phystech.edu>
2018-11-07 12:07:43 +0300

    switched to class

--- src/org/barabuga/build.groovy
+++ src/org/barabuga/build.groovy
@@ -1,27 +1,27 @@
 package org.barabuga;
 
-def buildUnix(label, stashName) {
-	node(label) {
-		checkout scm
-		sh """#!/bin/sh
-			gcc hello.c -o hello_${label}.out
-		"""
-		stash name: stashName, includes: "*.out"
-		deleteDir()
+class Build {
+	def buildUnix(label, stashName) {
+		node(label) {
+			checkout scm
+			sh """#!/bin/sh
+				gcc hello.c -o hello_${label}.out
+			"""
+			stash name: stashName, includes: "*.out"
+			deleteDir()
+		}
+	}
+
+	def buildWin(label, stashName) {
+		node(label) {
+			checkout scm
+			String vsvars_bat = 'Microsoft Visual Studio 12.0\\VC\\vcvarsall.bat'
+			bat """
+				call "%ProgramFiles(X86)%\\${vsvars_bat}" x86
+				cl.exe hello.c
+			"""
+			stash name: stashName, includes: "*.exe"
+			deleteDir()
+		}
 	}
 }
-
-def buildWin(label, stashName) {
-	node(label) {
-		checkout scm
-		String vsvars_bat = 'Microsoft Visual Studio 12.0\\VC\\vcvarsall.bat'
-		bat """
-			call "%ProgramFiles(X86)%\\${vsvars_bat}" x86
-			cl.exe hello.c
-		"""
-		stash name: stashName, includes: "*.exe"
-		deleteDir()
-	}
-}
-
-return this;
